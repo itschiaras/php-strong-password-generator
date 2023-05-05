363aae79e927abc4f8cd6b2ca63ed8359b1c7235
ChiaraS97 <chiara.sucato97@gmail.com>
2023-05-05 16:25:27 +0200

    milestone 1 completed

---
+++ index.php
@@ -0,0 +1,49 @@
+<!-- Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale -->
+
+<?php 
+
+$length = 0;
+function getRndPass($length) {
+    $length = $_GET['passLength'];
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#!$%&=?^*';
+    $shuffledchars = str_shuffle($chars);
+    $password = substr($shuffledchars, 0, $length);
+    return $password;
+}
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-center">Generatore di password <span class="text-uppercase">sicurissime (?)</span></h1>
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="GET" class="d-flex flex-column align-items-center">
+        <label for="passLength">Inserire la lunghezza desiderata per la password</label>
+        <input type="number" min="8" placeholder="min 8 caratteri" name="passLength" id="passLength">
+        <button type="submit">Crea</button>
+        <?php if (!empty($_GET['passLength']) && $_GET['passLength'] > 8) { ?>
+            <h1><?php echo getRndPass($length) ?></h1>
+        <?php } ?>
+        
+    </form>
+   
+</body>
+</html>
